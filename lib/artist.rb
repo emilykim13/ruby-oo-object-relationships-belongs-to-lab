7706b865d6ad3cc35c40005d9f06308eba812c08
Emily Nguyen <emilyn0614@gmail.com>
2021-02-25 13:01:59 -0600

    commit tree 261cf6c7ad20e283551f5d6c7c5df50f9983a7fd

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist
+    attr_accessor :name
+end
+
+
+
+
+
+# class Artist
+#     attr_accessor :name, :genre
+  
+#     def initialize(name, genre) #name and genre
+#       @name = name
+#       @genre = genre
+#     end
+  
+#   end
+  
+#   drake = Artist.new("Drake", "rap") 
+#   hotline_bling = Song.new("Hotline Bling")
+#   hotline_bling.artist = "Drake"    
+  
+#   hotline_bling.artist.name # => drake 
+#   hotline_bling.artist.genre # => rap
